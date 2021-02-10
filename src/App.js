6bd0d8d8ee4d2708188c03ee01226c5734a75eaf
Kevin Lacker <lacker@gmail.com>
2021-02-10 09:30:04 -0800

    selecting some words

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import choices from "./choices";
+import valid from "./valid";
+
+function choice(arr) {
+  let index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+}
 
 function App() {
+  let word = choice(choices);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {word}
+        <br />
+        VALID: {valid[word]}
       </header>
     </div>
   );
--- words/make.js
+++ words/make.js
@@ -10,12 +10,39 @@ async function loadText(fname) {
     crlfDelay: Infinity
   });
 
-  let count = 0;
+  let answer = [];
   for await (let line of rl) {
-    count += 1;
+    answer.push(line.trim().toLowerCase());
   }
-  console.log(`${count} lines in ${fname}`);
+  console.log(`${answer.length} lines in ${fname}`);
+  return answer;
 }
 
-loadText("collins2019.txt");
-loadText("google10k.txt");
+function saveJSON(fname, data) {
+  let content = `let data = ${JSON.stringify(data)};\nexport default data;`;
+  fs.writeFileSync(`${__dirname}/../src/${fname}`, content);
+}
+
+async function main() {
+  let collins = await loadText("collins2019.txt");
+  let common = await loadText("google10k.txt");
+
+  let valid = {};
+  for (let word of collins) {
+    valid[word] = 1;
+  }
+  saveJSON("valid.js", valid);
+  console.log(`wrote ${collins.length} valid words`);
+
+  let choices = [];
+  for (let word of common) {
+    if (!valid[word]) {
+      continue;
+    }
+    choices.push(word);
+  }
+  saveJSON("choices.js", choices);
+  console.log(`wrote ${choices.length} puzzle choices`);
+}
+
+main();
