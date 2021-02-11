4ee76ec86bed8af1024380a960dcea67720e5fb8
Kevin Lacker <lacker@gmail.com>
2021-02-11 13:25:00 -0800

    page title

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useEffect } from "react";
 
 import puzzles from "./puzzles";
 import solutions from "./solutions";
-// import valid from "./valid";
 
 function choice(arr) {
   let index = Math.floor(Math.random() * arr.length);
   return arr[index];
 }
 
+// In place
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
 function App() {
+  useEffect(() => {
+    let letters = Array.from("VALTINE");
+    shuffleArray(letters);
+    document.title = letters.join("");
+  }, []);
+
   let len = choice([3, 4, 5, 6, 7]);
   let letters = choice(puzzles[len]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        LEN: {len}
-        <br />
-        {letters}
-        <br />
-        SOLUTION: {choice(solutions[letters])}
-      </header>
+    <div className="h-screen v-screen flex flex-col">
+      <div className="flex-1">LEN: {len}</div>
+      <div className="flex-1">{letters}</div>
+      <div className="flex-1">SOLUTION: {choice(solutions[letters])}</div>
     </div>
   );
 }
